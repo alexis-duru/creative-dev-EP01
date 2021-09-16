9d955422e5a754fe7fa0f4afc353c64a8a8e3c46
alexis duru <alexis.duru.pro@gmail.com>
2021-09-16 11:27:45 +0200

    update v1 dark / light mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,22 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+// je pars en mode dark //
+
+const slider = document.querySelector('.slider');
+
+slider.addEventListener('click', () => {
+
+    const body = document.body;
+
+    if(body.classList.contains('dark')){
+
+        body.classList.add('light')
+        body.classList.remove('dark')
+        // toggleSwitch.innerHTML = "Go Dark"
+
+    } else if(body.classList.contains('light')){
+        body.classList.add('dark')
+        body.classList.remove('light')
+        // toggleSwitch.innerHTML = "Go Light"
+    } 
+    console.log(body.classList)
+        
+})
